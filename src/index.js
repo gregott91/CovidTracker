7cb1fa068e5453892626d6db0e11f4fba4870e8f
gregott91 <gregoryott2345@gmail.com>
2021-01-24 17:38:40 -0600

    Added logs

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import {startApp} from './root';
 import {transformData} from './data';
 
 async function start() {
+  console.log('Starting download');
   const data = await retrieveCovidData();
-  startApp(transformData(data));
+  console.log('Download complete, starting data transformation');
+  const transformed = transformData(data);
+  console.log('Transformation complete, starting app');
+  startApp(transformed);
+  console.log('App started');
 }
 
 start();
