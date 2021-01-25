e85db05bd9ee6a7428b1e647d018b8cdbbac7d4c
gregott91 <gregoryott2345@gmail.com>
2021-01-25 00:21:48 -0600

    Corrected card html

--- src/components/vue-datapoint.js
+++ src/components/vue-datapoint.js
@@ -6,23 +6,26 @@ export function initializeDatapointComponent() {
     template: `
 <div class="card datapoint-container">
   <div class="card-body">
-    <h3 class="card-title"><span class="card-title">{{ selecteddatatype }} for {{ fulldata[index].DisplayDate }}</span></h3>
+    <h3 class="card-title">{{ selecteddatatype }} for {{ fulldata[index].DisplayDate }}</h3>
     <div class="datapoint-primarydata">Total: {{ fulldata[index].Raw[selecteddatatype].TotalCount.Value }}</div>
     <div class="datapoint-primarydata">New: {{ fulldata[index].Raw[selecteddatatype].NewCount.Value }}</div>
-    <div class="datapoint-secondaryData">Change from last week: 
+    <div class="datapoint-secondaryData">
+      Change from last week: 
       <span v-bind:class="rawClass">{{ fulldata[index].Raw[selecteddatatype].NewCount.PercentChange }}</span>
     </div>
     <div class="datapoint-secondaryData">Value last week: {{ fulldata[index].Raw[selecteddatatype].NewCount.PreviousValue }}</div>
     <div class="datapoint-primarydata">Rolling average (7-day): {{ fulldata[index].Rolling[selecteddatatype].NewCount.Value }}</div>
     <div class="datapoint-secondaryData">Change from last week:
-    <div class="datapoint-secondaryData">Change from last week: 
+    <div class="datapoint-secondaryData">
+      Change from last week: 
       <span v-bind:class="rollClass">{{ fulldata[index].Rolling[selecteddatatype].NewCount.PercentChange }}</span>
     </div>
     <div class="datapoint-secondaryData">Value last week: {{ fulldata[index].Rolling[selecteddatatype].NewCount.PreviousValue }}</div>
     <div class="datapoint-primarydata">Expected next day: {{ fulldata[index].Predicted[selecteddatatype].NewCount.Value }}</div>
     <div v-if="fulldata[index].Predicted[selecteddatatype].NewCount.HasActual" class="datapoint-secondaryData">Actual: {{ fulldata[index].Predicted[selecteddatatype].NewCount.Actual }}</div>
+    </div>
   </div>
-</div>   
+</div>
     `,
     computed: {
       rawClass: function() {
