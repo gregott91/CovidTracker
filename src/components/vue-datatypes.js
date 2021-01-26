5d6f84e279ecd6ba9a21953d532d7d6ef2aa46f9
gregott91 <gregoryott2345@gmail.com>
2021-01-26 01:07:30 -0600

    Moved navbar to top

--- src/components/vue-datatypes.js
+++ src/components/vue-datatypes.js
@@ -5,14 +5,19 @@ export function initializeDataTypesComponent(observable) {
   Vue.component('vue-datatypes', {
     props: ['datatypes', 'selecteddatatype'],
     template: `
-<nav class="d-none d-md-block sidebar">
-  <div class="sidebar-sticky">
-    <ul class="nav flex-column">
-      <li class="nav-item" v-for="datatype in datatypes" v-on:click="dataTypeClicked" v-bind:datatype="datatype">
-        <div class="tab-inner selected-tab" v-if="datatype == selecteddatatype">{{ datatype }}</div>
-        <div class="tab-inner unselected-tab" v-else>{{ datatype }}</div>
-      </li>
-    </ul>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#top-navbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="top-navbar">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item" v-for="datatype in datatypes" v-on:click="dataTypeClicked" v-bind:datatype="datatype">
+          <div class="tab-inner selected-tab" v-if="datatype == selecteddatatype">{{ datatype }}</div>
+          <div class="tab-inner unselected-tab" v-else>{{ datatype }}</div>
+        </li>
+      </ul>
+    </div>
   </div>
 </nav>
 `,
