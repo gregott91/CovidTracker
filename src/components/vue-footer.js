a75d52964550c3292c43df4bbdf0782b891881d1
gregott91 <gregoryott2345@gmail.com>
2021-02-08 22:48:19 -0600

    Corrected footer

--- src/components/vue-footer.js
+++ src/components/vue-footer.js
@@ -11,7 +11,7 @@ export function initializeFooterComponent() {
         <a class="text-muted footer-text" href="https://github.com/gregott91/CovidTracker">See the source on GitHub</a>
       </div>
       <div class="col-sm-4">
-        <span class="text-muted footer-text">Built using data from The COVID Tracking Project</span>
+        <span class="text-muted footer-text">Built using data from Disease.sh</span>
       </div>
       <div class="col-sm-4">
         <span class="text-muted footer-text">Data last refreshed on: {{ retrievaltime }}</span>
